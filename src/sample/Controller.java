3b5eccb3779b014242bbf4b771668d185e8aa654
Emil <lort@gmail.com>
2020-10-29 13:57:36 +0100

    KUN EN XML

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,6 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -20,31 +22,60 @@ import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+import static java.lang.Thread.sleep;
+
+public class Controller {
 
 
     // socket attributes - >
     DataOutputStream toServer = null;
     DataInputStream fromServer = null;
-    Socket socket;
 
-    {
-        try {
-            if (socket == null) {
-                socket = new Socket("localhost", 8000);
-                System.out.println("connecting");
-                System.out.println(socket == null);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     // JAVAFX ELEMENTS
 
     public TextArea chatBox = new TextArea();
     public TextField usernameInput = new TextField();
     public TextField chatMessage = new TextField();
+    public HBox joinBox;
+
+    public void initialize() {
+        try {
+        Socket socket = new Socket("localhost", 8000);
+        toServer = new DataOutputStream(socket.getOutputStream());
+        fromServer = new DataInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // read thread
+            new Thread(() -> {
+                // LOBBY LOOP - >
+                while (true) {
+                    try {
+                        // READING A NEXT MESSAGE FROM THE SERVER AND APPENDING IT TO CHATBOX IN SCENE2
+                        chatBox.appendText(readMessage());
+                        if (readMessage().equalsIgnoreCase("START GAME")) {
+                            break;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+    }
+
+    public void joinTheServer(ActionEvent actionEvent) throws IOException {
+        // SENDING THE USERNAME TO THE SERVER USING THE SENDMESSAGE FUNCTION
+        sendMessage(usernameInput.getText());
+        joinBox.setVisible(false);
+    }
+
+
+    public void sendMessage(ActionEvent actionEvent) throws IOException {
+        sendMessage(chatMessage.getText());
+        // chatMessage.clear();
+    }
 
     public void sendMessage(String message) throws IOException {
         toServer.writeUTF(message);
@@ -52,65 +83,9 @@ public class Controller implements Initializable {
     }
 
     public String readMessage() throws IOException {
-        String m = null;
-        m = fromServer.readUTF();
-        return m;
+        return fromServer.readUTF();
     }
 
-    public void joinTheServer(ActionEvent actionEvent) throws IOException {
-        //CHANGING SCENE TO THE SCENE2.FXML WITH THE FOLLOWING CODE
-        Parent scene2Parent = FXMLLoader.load(getClass().getResource("scene2.fxml"));
-        Scene scene2 = new Scene(scene2Parent);
-        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene2);
-        stage.show();
 
-        // SENDING THE USERNAME TO THE SERVER USING THE SENDMESSAGE FUNCTION
-        String message = usernameInput.getText();
-        chatBox.appendText(message);
-        sendMessage(message);
-    }
 
-    public void sendMessage(ActionEvent actionEvent) throws IOException {
-        sendMessage(chatMessage.getText());
-        chatMessage.clear();
-
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        try{
-        // Create a socket to connect to the server
-
-        // Create an input stream to receive data from the server
-        fromServer = new DataInputStream(socket.getInputStream());
-        // Create an output stream to send data to the server
-        toServer = new DataOutputStream(socket.getOutputStream());
-
-        // read thread
-        new Thread(() -> {
-            try {
-                if (!chatBox.getText().isEmpty()){
-                chatBox.appendText(readMessage());}
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            // LOBBY LOOP - >
-            while(true){
-                try {
-                    // READING A NEXT MESSAGE FROM THE SERVER AND APPENDING IT TO CHATBOX IN SCENE2
-                    System.out.println("1");
-                    chatBox.appendText(readMessage());
-                    System.out.println("2");
-                    //if(readMessage().equalsIgnoreCase("START GAME")){
-                    //break;
-                    //}
-                } catch (IOException e) {
-                    e.printStackTrace(); }
-            }
-        }).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
     }
